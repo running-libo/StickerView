db49571d8d86e5b96c20afcda3b8b71b3c7a66ed
apple <18201497620@163.com>
2020-07-31 12:13:21 +0800

    initCommit

---
+++ app/src/main/java/com/example/stickerview/StickerView.java
@@ -0,0 +1,29 @@
+package com.example.stickerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import androidx.annotation.Nullable;
+
+/**
+ * create by libo
+ * create on 2020/7/31
+ * description 贴图view
+ */
+public class StickerView extends View {
+
+    public StickerView(Context context) {
+        super(context);
+        init();
+    }
+
+    public StickerView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+
+    }
+
+}
